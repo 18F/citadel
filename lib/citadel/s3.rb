1f8cc2a23560b48ada6c312edcaefc224dedf8e5
Andy Brody <git@abrody.com>
2017-07-29 17:23:45 -0400

    Implement v4 signatures for S3 calls.
    It would probably be better to use the aws-sdk or aws-sigv4 gems
    directly, but this will work for now.
    
    This allows Citadel to work with KMS encrypted data and with AWS regions
    that came online in 2014 or later.
    
    Fixes: #28

--- lib/citadel/s3.rb
+++ lib/citadel/s3.rb
@@ -51,34 +51,100 @@ class Citadel
     def get(bucket:, path:, access_key_id:, secret_access_key:, token: nil, region: nil)
       region ||= 'us-east-1' # Most buckets.
       path = path[1..-1] if path[0] == '/'
-      now = Time.now().utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
 
-      string_to_sign = "GET\n\n\n#{now}\n"
-      string_to_sign << "x-amz-security-token:#{token}\n" if token
-      string_to_sign << "/#{bucket}/#{path}"
+      verb = 'GET'
+      uri_path = "/#{bucket}/#{path}"
+      body = ''
 
-      signed = OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha1'), secret_access_key, string_to_sign)
-      signed_base64 = Base64.encode64(signed)
+      datetime = Time.now.utc.strftime('%Y%m%dT%H%M%SZ')
+      date = datetime[0,8]
+
+      c_scope = _credential_scope(date, region)
+      credential = "#{access_key_id}/#{c_scope}"
+
+      algorithm = 'AWS4-HMAC-SHA256'
+
+      if region == 'us-east-1'
+        hostname = 's3.amazonaws.com'
+      else
+        hostname = "s3-#{region}.amazonaws.com"
+      end
 
       headers = {
-        'date' => now,
-        'authorization' => "AWS #{access_key_id}:#{signed_base64}",
+        'host' => hostname,
+        'x-amz-content-sha256' => hexdigest(body),
+        'x-amz-date' => datetime,
+        'x-amz-expires' => '900', # 15 minutes
       }
       headers['x-amz-security-token'] = token if token
 
-      hostname = case region
-      when 'us-east-1'
-        's3.amazonaws.com'
-      else
-        "s3-#{region}.amazonaws.com"
-      end
+      canonical_request = _canonical_request(verb: verb, path: uri_path,
+                                             querystring: '', headers: headers,
+                                             content_hash: hexdigest(body))
+      signed_headers = headers.keys.sort.join(';')
+
+      to_sign = _string_to_sign(datetime, c_scope, canonical_request)
+      signed = _signature(secret_access_key, date, region, 's3', to_sign)
+
+      headers['authorization'] = "#{algorithm} Credential=#{credential}, SignedHeaders=#{signed_headers}, Signature=#{signed}"
+
+      # Debug information useful if the signature is wrong
+      Chef::Log.debug { "CanonicalRequest: " + canonical_request.inspect }
+      Chef::Log.debug { "StringToSign: " + to_sign.inspect }
+      Chef::Log.debug { "headers: " + headers.inspect }
 
       begin
-        Chef::HTTP.new("https://#{hostname}").get("#{bucket}/#{path}", headers)
+        Chef::HTTP.new("https://#{hostname}").get(uri_path[1..-1], headers)
       rescue Net::HTTPServerException => e
         raise CitadelError.new("Unable to download #{path}: #{e}")
       end
     end
 
+    def _canonical_request(verb:, path:, querystring:, headers:, content_hash:)
+      # This isn't a super robust way to calculate the canonical request, since
+      # we don't really deal properly with URIs that need escaping or quoted
+      # header values.
+      [
+        verb,
+        path,
+        querystring,
+        headers.sort_by(&:first).map {|k, v| "#{k}:#{v}" }.join("\n") + "\n", # sign all headers
+        headers.keys.sort.join(';'),
+        content_hash,
+      ].join("\n")
+    end
+
+    def _string_to_sign(datetime_string, credential_scope, canonical_request)
+      [
+        'AWS4-HMAC-SHA256',
+        datetime_string,
+        credential_scope,
+        hexdigest(canonical_request),
+      ].join("\n")
+    end
+
+    def _credential_scope(date_string, region, service='s3')
+      [date_string, region, 's3', 'aws4_request'].join('/')
+    end
+
+    def _signature(secret_access_key, date, region, service, string_to_sign)
+      k_date = hmac('AWS4' + secret_access_key, date)
+      k_region = hmac(k_date, region)
+      k_service = hmac(k_region, service)
+      k_credentials = hmac(k_service, 'aws4_request')
+      hmac_hex(k_credentials, string_to_sign)
+    end
+
+    def hexdigest(data)
+      OpenSSL::Digest::SHA256.hexdigest(data)
+    end
+
+    def hmac(key, value)
+      OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), key, value)
+    end
+
+    def hmac_hex(key, value)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), key, value)
+    end
   end
 end
